4787cded2c529609f73cf282cebc74f4fb41e8c0
Samyam Rajbhandari <samyamr@microsoft.com>
2019-04-08 14:12:21 -0700

    edits

--- setup.py
+++ setup.py
@@ -66,7 +66,8 @@ if "--cuda_ext" in sys.argv:
                                    'csrc/fused_lamb_cuda_kernel.cu'],
                           extra_compile_args={'cxx': ['-O3',],
                                               'nvcc':['-O3', 
-                                                      '--use_fast_math']}))
+                                                      '--use_fast_math',
+                                                      '-gencode arch=compute_60,code=sm_60 -gencode arch=compute_61,code=sm_61 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_75,code=sm_75 -gencode arch=compute_75,code=compute_75']}))
         
         ext_modules.append(
             CUDAExtension(name='syncbn',
