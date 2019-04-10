2539dfa9686dd371034c42e9ab02c0f5d8e267a5
Samyam Rajbhandari <samyamr@microsoft.com>
2019-04-10 16:29:28 -0700

    edits

--- tests/L0/run_mixed_adam/test_mixed_adam.py
+++ tests/L0/run_mixed_adam/test_mixed_adam.py
@@ -23,7 +23,7 @@ class TestFusedAdam(unittest.TestCase):
             tst_param.append(torch.nn.Parameter(tensor.clone()))
 
         #ref_optim = torch.optim.Adam(ref_param, **adam_option)
-        ref_optim = apex.optimizers.Lamb(ref_param, **adam_option)
+        ref_optim = Lamb(ref_param, **adam_option)
         tst_optim = Lamb(tst_param, **adam_option)
         print("FusedLamb")
        
