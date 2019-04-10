5d83b5fc552442966c87fdc84093485d707286e7
Samyam Rajbhandari <samyamr@microsoft.com>
2019-04-09 18:10:39 -0700

    edits

--- csrc/fused_lamb_cuda_kernel.cu
+++ csrc/fused_lamb_cuda_kernel.cu
@@ -240,7 +240,7 @@ __device__ void reduce_two_vectors_in_register(T a, T b, T* g_a, T* g_b){
     s_a[threadIdInBlock] = a;
     s_b[threadIdInBlock] = b;
 
-    if (tid == 0){
+    if (threadIdInBlock == 0){
         printf("Register function a %.3f. b %.3f \n",a, b);
     
     } 
